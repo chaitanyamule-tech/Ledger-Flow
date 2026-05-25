76f8a187beb6e03983107e7ab111455aa251a5f2
chaitanyamule-tech <mulec3640@gmail.com>
2026-05-25 22:43:43 +0530

    changes are done

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -12,21 +12,15 @@ const transporter = nodemailer.createTransport({
 });
 
 transporter.verify((error, success) => {
-
     if (error) {
-
         console.log('Email server error:', error);
-
     } else {
-
         console.log('Email server is ready to send messages');
     }
 });
 
 const sendEmail = async ({ to, subject, text, html }) => {
-
     try {
-
         const info = await transporter.sendMail({
             from: `"LedgerFlow" <${process.env.EMAIL_USER}>`,
             to,
@@ -34,28 +28,21 @@ const sendEmail = async ({ to, subject, text, html }) => {
             text,
             html
         });
-
         console.log('Message sent:', info.messageId);
-
         return info;
 
     } catch (error) {
-
         console.log('Error sending email:', error);
-
         throw error;
     }
 };
 
 const sendWelcomeEmail = async (userEmail, name) => {
-
     const subject = 'Welcome to LedgerFlow';
 
     const text = `
 Hi ${name},
-
 Welcome to LedgerFlow!
-
 Your account has been created successfully.
 
 Best Regards,
@@ -64,19 +51,14 @@ LedgerFlow Team
 
     const html = `
         <h2>Welcome to LedgerFlow</h2>
-
         <p>Hi ${name},</p>
-
         <p>Your account has been created successfully.</p>
-
         <p>We are excited to have you onboard.</p>
-
         <br>
 
         <p>Best Regards,</p>
         <p>LedgerFlow Team</p>
     `;
-
     await sendEmail({
         to: userEmail,
         subject,
